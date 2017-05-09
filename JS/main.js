83cf52efaa8977494a468f883b27d5b3304bd9b8
Vitalii Prots <vitalikprocj@gmail.com>
2017-05-09 18:39:53 +0300

    add-js_v3

---
+++ JS/main.js
@@ -0,0 +1,19 @@
+var userLastName = prompt("What is your last name?");
+var userLastNameElement = document.getElementById("lastName");
+userLastNameElement.innerText = userLastName;
+
+var userFirstName = prompt("What is your first name?");
+var userFirstNameElement = document.getElementById("firstName");
+userFirstNameElement.innerText = userFirstName;
+
+var userMiddleName = prompt("What is your middle name?");
+var userMiddleNameElement = document.getElementById("middleName");
+userMiddleNameElement.innerText = userMiddleName;
+
+var userGender = prompt("What is your gender?");
+var userGenderElement = document.getElementById("gender");
+userGenderElement.innerText = userGender;
+
+var userBirth = prompt("What is your birth date?");
+var userBirthElement = document.getElementById("birthDate");
+userBirthElement.innerText = userBirth;
